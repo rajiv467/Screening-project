3b4e410be5352579c966353a21773fa49124b476
rajiv467 <80842893+rajiv467@users.noreply.github.com>
2023-03-23 13:50:17 +0530

    Change region

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket = "screening-process-terraform-statefile"
     key    = "dev/project1-vpc/terraform.tfstate"
-    region = "ap-south-1" 
+    region = "us-east-1" 
     secret_key = "7tdZgSIaRMADpFMi5Ag2wD2YXVa//pfxMNK0zIkR"
     access_key = "AKIA4E5YNEF5ZL3QZE7A"  
 }
