37c01be5fb1a9713677ea0ffde28f952726d70f9
rajiv467 <80842893+rajiv467@users.noreply.github.com>
2023-03-23 14:53:33 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,13 +6,13 @@ terraform {
       version = "~> 3.0"
     }
   }
-#   backend "s3" {
-#     bucket = "screening-process-terraform-statefile"
-#     key    = "dev/project1-vpc/terraform.tfstate"
-#     region = "us-east-1" 
-#     secret_key = "7tdZgSIaRMADpFMi5Ag2wD2YXVa//pfxMNK0zIkR"
-#     access_key = "AKIA4E5YNEF5ZL3QZE7A"  
-# }
+  backend "s3" {
+     bucket = "screening-process-terraform-statefile"
+     key    = "dev/project1-vpc/terraform.tfstate"
+     region = "us-east-1" 
+     secret_key = "7tdZgSIaRMADpFMi5Ag2wD2YXVa//pfxMNK0zIkR"
+     access_key = "AKIA4E5YNEF5ZL3QZE7A"  
+ }
 }
 # Configure the AWS Provider
 provider "aws" {
